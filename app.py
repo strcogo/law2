403ae59a061f4928e235878973dcf7d09233fc38
Pedro Cougo <160169155+strcogo@users.noreply.github.com>
2024-08-16 08:37:25 -0300

    das

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "9970626666560a32465d4ce10d28f3233365af833e15eed59884d9477862c379"
 
 @app.route('/')
 def index():
@@ -13,6 +14,16 @@ def index():
 def aula(name = "juan", curso = "informatica", ano = 2):
     dados = {'name':name, 'curso':curso, 'ano':ano}
     return render_template("aula.html", dados_curso = dados)
+
+@app.route("/form")
+def form():
+    return render_template("form.html")
+
+@app.route("/dados", methods=["POST"])
+def dados():
+    flash("dados enviados!!")
+    dados = request.form
+    return render_template("dados.html", dados=dados)
 
 if __name__ == '__main__':
     app.run()
